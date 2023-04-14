c22ae9747e8d75722a7cb24fa21380626c602e4e
filomba01 <filippo.balzarini@mail.polimi.it>
2023-04-14 12:31:01 +0200

    implemented msg and services

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/filippo/ros_proj_1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/filippo/robotics/devel;/opt/ros/noetic'.split(';') if '/home/filippo/robotics/devel;/opt/ros/noetic' != '' else []
--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/filippo/ros_proj_1/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
