7a978297ac09410c56feaa556579e87d9ac38b5e
filomba01 <filippo.balzarini@mail.polimi.it>
2023-06-24 13:33:47 +0200

    improved move base navigation, bugs to be resolved

--- src/second_project/src/navigation.cpp
+++ src/second_project/src/navigation.cpp
@@ -12,22 +12,28 @@
 #define X_POS 0
 #define Y_POS 1
 #define W_POS 2 
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
 class Navigation{
     private:
-
-        ros::NodeHandle n;
+	
+	ros::NodeHandle nh;
 
         void move_to_goal(double x, double y, double tetha){
-            ros::NodeHandle nh;
+            
+            ROS_INFO("MoveBase parameters recived to be reached: X: %f , Y: %f, W: %f",x,y,tetha);
             MoveBaseClient client("move_base", true);
 
             ROS_INFO("Waiting for the move_base action server...");
-            client.waitForServer();
+            while(!client.waitForServer(ros::Duration(5.0))){
+		    ROS_INFO("Waiting for the move_base action server to come up");
+	    }
 
             move_base_msgs::MoveBaseGoal goal;
-
-            goal.target_pose.header.frame_id = "map";  
+   	    ROS_INFO("Server Ready!");
+            goal.target_pose.header.frame_id = "base_link"; 
+            goal.target_pose.header.stamp = ros::Time::now(); 
             goal.target_pose.pose.position.x = x;    
             goal.target_pose.pose.position.y = y;    
             goal.target_pose.pose.orientation.w = tetha; 
@@ -51,8 +57,9 @@ class Navigation{
         void init(){
             std::string waypoint_file;
             ros::param::get("waypoint_file", waypoint_file);
+            ROS_INFO("Node init!");
+
             this->readData(waypoint_file);
-            ros::spin();
             return;
         }
 
@@ -60,10 +67,10 @@ class Navigation{
 
                     std::ifstream file(filename);
                     std::string line;
-
+		    ROS_INFO("searching waypoints!");
                     if (!file.is_open())
                     {
-                        ROS_INFO("Waipoint file is missing!");
+                        ROS_INFO("Waypoint file is missing!");
                         return;
                     }
 
@@ -73,13 +80,13 @@ class Navigation{
                         std::vector<std::string> row;
                         std::stringstream ss(line);
                         std::string cell;
-
+			ROS_INFO("computing waypoints!");
                         while (std::getline(ss, cell, ','))
                         {
                             row.push_back(cell);
                         }
-
-                        this->move_to_goal(std::stoi(row.at(X_POS)),std::stoi(row.at(Y_POS)),std::stoi(row.at(W_POS)));
+			ROS_INFO("GOAL to be reached: X: %f , Y: %f, W: %f",std::stod(row.at(X_POS)), std::stod(row.at(Y_POS)), std::stod(row.at(W_POS)));
+                        this->move_to_goal(std::stod(row.at(X_POS)),std::stod(row.at(Y_POS)),std::stod(row.at(W_POS)));
 
                     }
 
